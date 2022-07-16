9819600154ccf3bc1fa8ea33a60a573036e0f7b1
OliveiraDCI <oliveira.sein.dci@gmail.com>
2022-07-16 16:19:22 +0200

    Exercise concluded.

--- index.js
+++ index.js
@@ -2,18 +2,20 @@
 
 // Otherwise returns false:
 
-
 function checkAge(age) {
-
-  }
-
+  return age > 18 ? "true" : "false";
+}
 
 // Write a function min(a,b) which returns the least of two numbers a and b.
-  function min(a, b) {
-   
-  }
+function min(a, b) {
+  return a < b ? a : b;
+}
 
 // Write a function pow(x,n) that returns x in power n. Or, in other words, multiplies x by itself n times and returns the result.
-  function pow(x, n) {
-    
-  }
+function pow(x, n) {
+  return x ** n;
+}
+
+console.log("1.", checkAge(19));
+console.log("2.", min(2, 5));
+console.log("3.", pow(3, 2));
